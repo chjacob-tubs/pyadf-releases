aae7b16e312da2365b8e9587acc327e01873edda
Christoph Jacob <c.jacob@tu-braunschweig.de>
2026-09-11 15:12:44 +0200

    Release v1.6.1

--- src/pyadf/test/hydrogen_solver.py
+++ src/pyadf/test/hydrogen_solver.py
@@ -16,12 +16,12 @@
 
 # This file is part of
 # PyADF - A Scripting Framework for Multiscale Quantum Chemistry.
-# Copyright (C) 2006-2024 by Christoph R. Jacob, Tobias Bergmann,
+# Copyright (C) 2006-2026 by Christoph R. Jacob, Tobias Bergmann,
 # S. Maya Beyhan, Julia Brüggemann, Rosa E. Bulo, Maria Chekmeneva,
 # Thomas Dresselhaus, Kevin Focke, Andre S. P. Gomes, Andreas Goetz,
-# Michal Handzlik, Karin Kiewisch, Moritz Klammler, Lars Ridder,
-# Jetze Sikkema, Lucas Visscher, Johannes Vornweg, Michael Welzel,
-# and Mario Wolter.
+# Michal Handzlik, Karin Kiewisch, Moritz Klammler, Toni M. Maier, 
+# Malgorzata Olejniczak, Lars Ridder, Jetze Sikkema, Erik Tsvetaev, 
+# Lucas Visscher, Johannes Vornweg, Michael Welzel, and Mario Wolter.
 #
 #    PyADF is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
